0b2b14347c4efbf5757f557bd103672cd8ab9891
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-07-09 15:33:30 -0400

    Created exercise2

---
+++ exercise2.py
@@ -0,0 +1,18 @@
+#Question 1
+meal = 55 # shows the price of the meal
+tip = 5 # shows the tip amount
+print("{}".format(55 + 5)) # 60 (the total price of the meal)
+
+#Question 2
+"Like" + 1 # trying to add a string and an integer
+"Like" + "1" # converting the integer to a string and adding them
+print("Like" + "1") # Like1
+
+#Question 3
+45628 # first value to multiply
+7839 # second value to multiply
+print("45628 times 7839" + " is {}".format(45628 * 7839))
+# 45628 times 7839 is 357677892
+
+#Question 4
+print((10 < 20 and 30 < 20) or not(10 == 11)) # true
